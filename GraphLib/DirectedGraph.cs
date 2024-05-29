b7869c74a38b4ade24bf7f6f9228cf187d67a55f
GiftedR <kidsgamers@hotmail.com>
2024-05-29 11:53:56 -0700

    Added Dijkstra

--- GraphLib/DirectedGraph.cs
+++ GraphLib/DirectedGraph.cs
@@ -1,4 +1,6 @@
-﻿namespace GraphLib
+﻿using System.Numerics;
+
+namespace GraphLib
 {
     public class DirectedGraph
     {
@@ -13,9 +15,9 @@
             return v;
         }
 
-        public D?[,] CreateAdjacencyMatrix<D>()
+        public D[,] CreateAdjacencyMatrix<D>()
         {
-            D?[,] adjMatrix = new D?[activeVerticies.Count,activeVerticies.Count];
+            D[,] adjMatrix = new D[activeVerticies.Count,activeVerticies.Count];
 
             for (int vx = 0; vx < vertexCount; vx++)
             {
@@ -56,6 +58,68 @@
                 }
                 Console.WriteLine();
             }
+        }
+    
+        public void Dijkstra<D>(D[,] matrix, int start) where D: INumber<D>
+        {
+            // Gib Buckets
+            D[] distance = new D[vertexCount];
+            bool[] history = new bool[vertexCount];
+            D inf = (D)Convert.ChangeType(int.MaxValue, typeof(D));
+
+            Console.WriteLine($"Infinity is {inf}");
+
+            // Fill me buckets
+            for (int i = 0; i < vertexCount; i++)
+            {
+                distance[i] = inf;
+                history[i] = false;
+            }
+
+            distance[start] = default(D);
+
+            for (int count = 0; count < vertexCount - 1; count++)
+            {
+                int u = MinDistance<D>(distance, history);
+
+                history[u] = true;
+
+                for (int v = 0; v < vertexCount; v++)
+                {
+                    if (!history[v] &&
+                        matrix[u, v] != default(D) &&
+                        distance[u] != inf &&
+                        distance[u] + matrix[u, v] < distance[v]
+                        )
+                    {
+                        distance[v] = distance[u] + matrix[u, v];
+                    }
+                }
+            }
+
+            PrintSolution(distance, vertexCount);
+        }
+
+        private void PrintSolution<D>(D[] distance, int vertexCount) where D : INumber<D>
+        {
+            throw new NotImplementedException();
+        }
+
+        private int MinDistance<D>(D[] distance, bool[] history) where D : INumber<D>
+        {
+            D min = (D)Convert.ChangeType(int.MaxValue, typeof(D));
+            int min_index = -1;
+
+            for (int i = 0; i < vertexCount; i++)
+            {
+                if (!history[i] && distance[i] <= min)
+                {
+                    min = distance[i];
+                    min_index = i;
+                }
+            }
+
+            return min_index;
         }
     }
 }
